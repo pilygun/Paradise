07b5c08c75894fa5efa65f411a270fb2874ca110
pilygun <dfyz47@gmail.com>
2025-08-01 17:45:03 +0700

    Revert "z"
    This reverts commit 55b3620a1231c3ea59dc50d9d7b99b79e8ea1d79.

--- rust/src/uuid/statics.rs
+++
@@ -1,7 +0,0 @@
-use byondapi::value::ByondValue;
-use slotmap::{DefaultKey, SlotMap};
-use std::cell::RefCell;
-
-thread_local! {
-    pub static UUID_STORAGE: RefCell<SlotMap<DefaultKey, ByondValue>> = RefCell::new(SlotMap::with_capacity(100_000));
-}
